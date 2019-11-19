02fb5a29ccb3f9789fb6710addfc9e9a4513e745
Lishang-ichigaya <kuromitsu0528@gmail.com>
2019-11-19 13:18:04 +0900

    生成行列の訂正

---
+++ Simple-Polar-Code/polarcode/codeword.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def GetGeneratorMatrix(M):
+    """
+    ポーラ符号の生成行列を作成 
+    M: 符号長Nについて、N=2^Mを満たすM
+    """
+    matrixF_2 =np.matrix([[1,0],[1,1]])
+    matrixF = np.matrix([[1,0], [1,1]])
+    matrixB = GetPermutationMatrix(M)
+    for i in range(M-1):
+        matrixF = np.kron(matrixF_2, matrixF)
+    matrixG = np.dot(matrixB, matrixF)
+    return matrixG
+
+
+def GetPermutationMatrix(M):
+    """
+    偶数番目を前に、奇数番目を後ろに置き換える行列を得る
+    M: 符号長Nについて、N=2^Mを満たすM
+
+    例:
+    [1,0,0,0]  [1,0,0,0]
+    [0,1,0,0]->[0,0,1,0]
+    [0,0,1,0]  [0,1,0,0]
+    [0,0,0,1]  [0,0,0,1]
+    """
+    matrixI_2 = np.matrix([[1, 0], [0, 1]])
+    matrixB = np.matrix([[1, 0], [0, 1]])
+    for i in range(M-1):
+        matrixB = np.kron(matrixI_2, matrixB)
+    matrixEven, matrixOdd = matrixB[::2], matrixB[1::2]
+    matrixB = np.concatenate([matrixEven, matrixOdd]).T
+    return matrixB
+
+tt = GetGeneratorMatrix(3)
+print(tt)
